6804bd0c944fd2ef7db8314957cd47f10b214889
Victor Garcia <victor.garcia@cimat.mx>
2020-09-11 19:31:10 -0500

    Arreglo temporal de contacto 14 con JS

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('#bt1').click(function() {
         $('#form1').attr('action',
-                       'mailto:victor.garcia@cimat.mx?subject=Hola, soy ' +
-                       $('#name').val() + '&body=' + $('#consulta').val());
+                       'mailto:victor.garcia@cimat.mx?subject=Hola, soy &body= ¿Que?' +
+//                       $('#name').val() + '&body=' + $('#consulta').val());
         $('#form1').submit();
     });
 });
