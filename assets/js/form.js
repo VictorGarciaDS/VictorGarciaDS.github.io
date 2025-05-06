2fb1be859421c4a89654752505c1279bb14a5791
VictorGarciaDS <69226132+VictorGarciaDS@users.noreply.github.com>
2025-05-05 21:06:13 -0600

    Delete assets directory

--- assets/js/form.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#bt1').click(function() {
-        $('#form1').attr('action',
-                       'mailto:victor.garcia@cimat.mx?subject=' +
-                       $('#name').val() + '&body=' + $('#consulta').val());
-        $('#form1').submit();
-    });
-});
